695c2ea87acc354ce69b51c21b7800b552a24009
sheldon92 <sam53@njit.edu>
2013-10-16 21:45:18 -0400

    Several updates to all files

--- Sheldon's Stuff/compile.php
+++ Sheldon's Stuff/compile.php
@@ -12,22 +12,17 @@ curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
 curl_setopt($ch, CURLOPT_POST, 1);
 curl_setopt($ch, CURLOPT_POSTFIELDS, "bsh.script=".$script."");
 
-
 $out = curl_exec($ch);
-
-
-
-
 curl_close($ch);
-
 
 $tag = '/<pre>(.*?)<\/pre>/s';
 if (preg_match($tag, $out, $list))
-	echo $list[0];
+	$_SESSION['answer'] = $list[0];
 else
-	$tag = '/<td bgcolor="#eeeeee">(.*?)<\/td>/s';
-	preg_match($tag, $out, $list);
-	echo $list[0];
+
+$tag = '/<td bgcolor="#eeeeee">(.*?)<\/td>/s';
+if(preg_match($tag, $out, $list));
+	$_SESSION['error'] = $list[0];
 
 ?>
 
